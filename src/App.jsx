bf2ba8e8a040bfe43b1dccc9077d1cd1ba243726
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2025-06-16 21:02:52 +0600

    created square component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-6xl font-bold">Welcome to my tic tac toe game.</h1>
+      <h1 className="text-6xl font-bold">Welcome to my tic tac toe game mf.</h1>
     </>
   );
 }
---
+++ src/components/board/Square.jsx
@@ -0,0 +1,9 @@
+export default function Square() {
+  return (
+    <div>
+      <button className="w-24 h-24 bg-gray-200 border border-gray-400 rounded-lg text-2xl font-bold">
+        X
+      </button>
+    </div>
+  );
+}
